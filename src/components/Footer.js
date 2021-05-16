0f4656b4598d7b345871f8821155ff23f9083499
John Michael Culaban <culabanjohnmichael@gmail.com>
2021-05-16 12:13:02 +0800

    add: Footer indicator with animation for Large screens

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="tap-indication">Hover mouse to words</div>
+  )  
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Intro from '../components/Intro';
 import Profile from '../components/Profile';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Intro />
+      <Footer />
       <Profile />
+      
     </div>
   );
 };
