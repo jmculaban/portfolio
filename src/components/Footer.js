6c56540425b163525fa069356d50e5775f1fdb31
John Michael Culaban <culabanjohnmichael@gmail.com>
2021-05-16 14:09:00 +0800

    update: Add indication for mobile and small screen devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="tap-indication">Hover mouse to words</div>
+    <div>
+      <div className="tap-indication">Hover mouse to words</div>
+      <div className="mobile-indication">Tap anywhere</div>
+    </div>
   )  
 }
 
