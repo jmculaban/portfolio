73e60b2d4d6f1c39eb0f06e7e884462d3b458f3c
John Michael Culaban <culabanjohnmichael@gmail.com>
2021-05-19 10:44:32 +0800

    update: Import pages link in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './pages/Home';
 import Work from './pages/Work';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Container from 'react-bootstrap/Container';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
@@ -11,7 +13,9 @@ function App() {
         <Container>
           <Switch>
             <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
             <Route exact path='/work' component={Work} />
+            <Route exact path='/contact' component={Contact} />
           </Switch>
         </Container>
       </BrowserRouter>
