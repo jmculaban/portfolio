3d7d2fe56d83c9f55a09449904feb6d19a39589f
John Michael Culaban <culabanjohnmichael@gmail.com>
2021-05-22 15:14:44 +0800

    add: Create favicon then finalize about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,9 +10,11 @@ const About = () => {
         <h2>Full Stack Web Developer</h2>
         <div className='max-width-inner about-content'>
           <h4>About</h4>
-          <p>Hi, I'm Michael. Experienced Electronics Engineer in the field of telecommunications. Having the responsibility of the technical operations, created some solutions that can help the company. Created some applications that automate the inventory management, generate a summary of daily accomplishment report and other solutions that help decrease some unnecessary workload. It inspires me to look into software development career.</p>
-          <p>Joined the bootcamp of Zuitt for Full Stack Web Developer. Learned to build web applications using MERN stack. With the help of Zuitt and some self-learnings, develop the following skills:</p>
+          <p>Hi, I'm Michael. Experienced Electronics Engineer in the field of telecommunications. Designated to lead a department, created some solutions like applications that automate the inventory management, generation of summary of daily accomplishment report and other solutions that help decrease some unnecessary workload that will help the company. It inspires me to look into a software development career.</p>
+          <p>Joined the boot camp of Zuitt for Full Stack Web Developer. Learned to build web applications using the MERN stack. With the help of Zuitt and some self-learnings, develop the following skills:</p>
           <Skills />
+          <p>You can download and check my resume in the link below:</p>
+          <a className='social-links resume' href='https://drive.google.com/file/d/1S480ZstR1fVTDya5XmY2Yne6ecBSZcgT/view?usp=sharing' target='_blank' rel='noreferrer'>My Resume</a>
         </div>
       </div>
     </div>
